8011cf5120b483ae03809cb59b50be963c69c946
mil2ne <show3070@gmail.com>
2020-06-10 21:47:29 +0900

    command 이용

---
+++ rooms/management/commands/seed_amenities.py
@@ -0,0 +1,61 @@
+from django.core.management.base import BaseCommand
+from rooms import models as room_models
+
+
+class Command(BaseCommand):
+    help = "This command tells me that we loves me"
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--times", help="how many times",
+        )
+
+    def handle(self, *args, **options):
+        amenities = [
+            "Air conditioning",
+            "Alarm Clock",
+            "Balcony",
+            "Bathroom",
+            "Bathtub",
+            "Bed Linen",
+            "Boating",
+            "Cable TV",
+            "Carbon monoxide detectors",
+            "Chairs",
+            "Children Area",
+            "Coffee Maker in Room",
+            "Cooking hob",
+            "Cookware & Kitchen Utensils",
+            "Dishwasher",
+            "Double bed",
+            "En suite bathroom",
+            "Free Parking",
+            "Free Wireless Internet",
+            "Freezer",
+            "Fridge / Freezer",
+            "Golf",
+            "Hair Dryer",
+            "Heating",
+            "Hot tub",
+            "Indoor Pool",
+            "Ironing Board",
+            "Microwave",
+            "Outdoor Pool",
+            "Outdoor Tennis",
+            "Oven",
+            "Queen size bed",
+            "Restaurant",
+            "Shopping Mall",
+            "Shower",
+            "Smoke detectors",
+            "Sofa",
+            "Stereo",
+            "Swimming pool",
+            "Toilet",
+            "Towels",
+            "TV",
+        ]
+
+        for a in amenities:
+            room_models.Amenity.objects.create(name=a)
+        self.stdout.write(self.style.SUCCESS("Amenities created!"))
